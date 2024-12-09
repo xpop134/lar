4648a04b94748d38a5d1726eef7541e4018b5dc7
xpop134 <127761568+xpop134@users.noreply.github.com>
2024-12-09 20:46:30 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
             'port' => env('AZURE_MYSQL_PORT', '3306'),
             'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
-            'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
+            'username' => env('AZURE_MYSQL_USERNAME', 'root'),
             'password' => env('AZURE_MYSQL_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -135,11 +135,13 @@ return [
         ],
 
         'cache' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'database' => env('AZURE_REDIS_DATABASE', '1'),
         ],
 
     ],
